98c90381935c5128f8a82ba0e64420f0ba7da236
Aaron Bieber <deftly@gmail.com>
2013-02-11 13:29:00 -0700

    initial commit of external ip server

---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require( 'http' ); 
+var server = http.createServer( function( req, res ) {
+  var from = '';
+
+  if ( req.connection.remoteAddress === '127.0.0.1' || req.connection.remoteAddress === 'localhost' ) { 
+    from = req.headers['x-forwarded-for'];
+  } else {
+    from = req.connection.remoteAddress;
+  }
+
+  res.end( from + "\n" );
+// }).listen( 3013, '206.125.173.186' );
+}).listen( 3013 );
