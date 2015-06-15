eff0bf4c6eb609f361a5e1f982b9da4053072907
Aaron Bieber <deftly@gmail.com>
2015-06-15 16:40:49 -0600

    pew

--- ip.js
+++ ip.js
@@ -1,23 +1,11 @@
-var http = require( 'http' ),
-geoip = require( 'geoip-lite' ),
-server = http.createServer( function( req, res ) {
-  var from = '', geo;
+var http = require( 'http' ); 
+var server = http.createServer( function( req, res ) {
+  var from = req.connection.remoteAddress || req.headers['x-forwarded-for'] || 'unknown';
 
   if ( req.connection.remoteAddress === '127.0.0.1' || req.connection.remoteAddress === 'localhost' ) { 
-    from = req.headers['x-forwarded-for'] || '127.0.0.1';
-  } else {
-    from = req.connection.remoteAddress;
+    from = req.headers['x-forwarded-for'];
   }
 
-  if ( req.url === '/geo' ) {
-    geo = geoip.lookup( from ) || {};
-    geo.ip = from;
-    res.end( JSON.stringify(geo) );
-  } else if ( req.url === '/mirror' ) {
-    geo = geoip.lookup( from ) || {};
-    // lookup in the mirror db 
-  } else {
-    res.end( from + "\n" );
-  }
-
+  res.end( from + "\n" );
 }).listen( 3013 );
+
